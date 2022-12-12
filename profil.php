a03f7d4c0524c0e71e1c04b946988d793355a149
axel-vair <100695867+axel-vair@users.noreply.github.com>
2022-12-12 11:32:56 +0100

    add avoid change login with login that exist

--- profil.php
+++ profil.php
@@ -4,32 +4,50 @@ include 'header.php';
 
 if(!empty($_SESSION)) {
     $login = $_SESSION['login'];
-    $sql = "SELECT * FROM utilisateurs WHERE login = :login";
+    $sql = 'SELECT * FROM utilisateurs WHERE login = :login';
     $sql_exe = $conn->prepare($sql);
-    $sql_exe->bindParam(":login", $login); // bind value to placeholder
-    $sql_exe->execute(); // execute query
-    $results = $sql_exe->fetch(PDO::FETCH_ASSOC); // fetch results in assoc array
-    $login = $results['login'];
+    $sql_exe->bindParam(':login', $login);
+    $sql_exe->execute();
+    $results = $sql_exe->fetch(PDO::FETCH_ASSOC);
     $password = $results['password'];
 
-    if(isset($_POST['submit'])){
-        if($login != $_POST['login']){
-            $sql1 = "UPDATE utilisateurs SET login='{$_POST['login']}' WHERE login= :login";
+    if (isset($_POST['submit'])) {
+        $sql2 = 'SELECT COUNT(*) FROM utilisateurs WHERE login = :login';
+        $sql_exe2 = $conn->prepare($sql);
+        $sql_exe2->bindParam(':login', $_POST['login']);
+        $sql_exe2->execute();
+        $results2 = $sql_exe2->fetchAll(PDO::FETCH_ASSOC);
+
+        if(!empty($results2)){
+            echo "<p id='error'>Le login est déjà utilisé</p>";
+
+        } else {
+            $sql1 = "UPDATE utilisateurs SET login='{$_POST['login']}' WHERE login = :login";
             $sql1_exe = $conn->prepare($sql1);
             $sql1_exe->bindParam(":login", $login);
             $sql1_exe->execute();
             $_SESSION['login'] = $_POST['login'];
-            echo "Votre login a bien été changé par" . $_POST['login'] ."<br>";
+            echo "<p id='valid'>Votre login a bien été changé par " . $_POST['login'] . "</p>" . "<br>";
 
-        } if($password != $_POST['password']){
+        }
+        if ($password != $_POST['password']) {
             $new_password = password_hash($_POST['password'], PASSWORD_DEFAULT);
             $sql2 = "UPDATE utilisateurs SET password='$new_password' WHERE password= :password";
             $sql2_exe = $conn->prepare($sql2);
             $sql2_exe->bindParam(":password", $password);
             $sql2_exe->execute();
-            echo "Votre login a bien été changé par" . $_POST['password'] ."<br>";
+            echo "<p id='valid'>Votre mot de passe a bien été changé</p>";
         }
         header('Refresh: 1; url=profil.php');
+    }
+
+
+    if (isset($_POST['delete'])) { // if user push delete button so delete all data from bdd and destroy session then redirect.
+        $sql_delete = "DELETE FROM utilisateurs WHERE login='$login'";
+        $result_delete = $conn->query($sql_delete);
+        echo "Vos données ont été supprimées";
+        session_destroy();
+        header('Location: index.php');
     }
 }
 ?>
